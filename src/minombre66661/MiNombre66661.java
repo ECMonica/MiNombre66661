06f287062752d30da232e45a1ba97c146153e550
cj_be <cj_be@DESKTOP-7ENKCBI>
2021-02-25 20:35:24 -0600

    Mi primer commit

---
+++ src/minombre66661/MiNombre66661.java
@@ -0,0 +1,22 @@
+/*
+ * Lenguajes de programación I
+ * Facultad de Ingeniería
+ * Universidad Autónoma de Campeche
+ */
+
+package minombre66661;
+
+/**
+ *
+ * @author Monica Ehuan Cisneros
+ */
+public class MiNombre66661 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Mi nombre es Monica de los Angeles Ehuan Cisneros");
+    }
+    
+}
